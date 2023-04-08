5a1e2456d80f003ef065ede3ff74e4efd06778cc
VIVEK KUMAR SAHU <113853773+viveksahu06@users.noreply.github.com>
2023-04-09 02:31:55 +0530

    Update RecursionTowerOfHanoi.java

--- RecursionTowerOfHanoi.java
+++ RecursionTowerOfHanoi.java
@@ -1,3 +1,13 @@
+/*Tower of Hanoi is a mathematical puzzle where we have three rods (A, B, and C) and N disks.
+Initially, all the disks are stacked in decreasing value of diameter i.e., the smallest disk 
+is placed on the top and they are on rod A. The objective of the puzzle is to move the entire
+stack to another rod (here considered C), obeying the following simple rules: 
+Only one disk can be moved at a time.
+Each move consists of taking the upper disk from one of the stacks and placing it on top of 
+another stack i.e. a disk can only be moved if it is the uppermost disk on a stack.
+No disk may be placed on top of a smaller disk.*/
+
+
 public class RecursionTowerOfHanoi { 
     public static void towerOfHanoi(int n,String src,String helper,String dest){
         if(n==1){
@@ -12,7 +22,7 @@ public class RecursionTowerOfHanoi {
         towerOfHanoi(n-1,helper,src,dest);
     }
     public static void main(String []args){
-           int n=2; 
+           int n=3; 
             towerOfHanoi(n,"A","B","C");
     }
 }
