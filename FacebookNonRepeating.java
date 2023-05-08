ab440c731aeadfe6c49f979980ffe8dcc654146b
VIVEK KUMAR SAHU <113853773+viveksahu06@users.noreply.github.com>
2023-05-08 11:22:18 +0530

    Add files via upload

---
+++ FacebookNonRepeating.java
@@ -0,0 +1,40 @@
+public class FacebookNonRepeating { 
+ /*  public static int nonRepeatingIndex(String s){
+        if(s.length()==1) return 0;  
+       
+        for(int i=0;i<s.length();i++){
+            char ab=s.charAt(i);  
+            boolean b=false;
+            for(int j=i+1;j<s.length();j++){
+                if(ab==s.charAt(j))  {
+                    b=true; 
+                    break;
+                } 
+                
+            } 
+            if(b==false) return i;
+        } 
+        return -1;
+    }*/ 
+     public static void main(String []args){
+      //  String s="abacdcbd"; 
+         // System.out.println(nonRepeatingIndex(s));
+       //  public static void main(String args[]) {
+
+        int x = 10;
+
+        int y = 20;
+      
+        int z = 30;
+      
+        if (x+y%z > (x+(-y)*(-z))) {
+      
+        System.out.println(x + y + z);
+      
+        }
+      
+        
+            
+         //   }
+     }
+}
